742afa24b77b6015310effd180782ccbf8308314
wanderhungerbuhler <whfdev@gmail.com>
2020-11-26 00:27:41 -0300

    New Website in NextJS

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PULIC_NODEMAILER_URL}/api`,
+  baseURL: `${process.env.NODEMAILER_URL}/api`,
 });
 
 export default api;
